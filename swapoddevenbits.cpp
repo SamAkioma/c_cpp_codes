f5ce368c2c7db070e72b7ee24739c152f44b7ae9
Sampreeth <vsampreeth.nitt@gmail.com>
2026-01-12 21:46:18 +0530

    Create swapoddevenbits.cpp

---
+++ swapoddevenbits.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+unsigned int swapBits(unsigned int n) {
+    unsigned int oddmask = 0xaaaaaaaa;
+    unsigned int evenmask = 0x55555555;
+    unsigned int oddbits = (n&oddmask)>>1;
+    unsigned int evenbits = (n&evenmask)<<1;
+    return (oddbits | evenbits);
+}
+
+int main(){
+  unsigned int num;
+  scanf("%u", &num);4
+  unsigned int result = swapbits(num);
+  printf("%u", result);
+  return 0;
+}
